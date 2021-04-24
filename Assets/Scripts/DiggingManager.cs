b327e1a392c17879f2d667829879e7d08c4c61cc
Stefan Laimer <s.laimer@str8labs.com>
2021-04-24 13:10:48 +0200

    Doozy + DiggingManager

---
+++ Assets/Scripts/DiggingManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiggingManager : MonoBehaviour
+{
+    public int Gems;
+    public int Dwarves;
+    
+    public int Depth;
+    public List<int> DepthCosts;
+    
+    //Forge Upgrades
+    public float Quality = 1f;
+    //Tavern Upgrades
+    public float Speed = 1f;
+
+
+    public static DiggingManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(this);
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        StartCoroutine(Mining());
+    }
+
+    public void IncreaseDepth()
+    {
+        if (Gems >= DepthCosts[Depth] && Depth < DepthCosts.Count - 1)
+        {
+            Gems -= DepthCosts[Depth];
+            Depth++;
+        }
+    }
+
+    public void PayGems(int amount)
+    {
+        Gems -= amount;
+    }
+
+    public void IncreaseDwarves(int amount)
+    {
+        Dwarves += amount;
+    }
+
+    private IEnumerator Mining()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f / Speed);
+            //Todo: Balancing
+            Gems += Mathf.RoundToInt(Dwarves * Quality * Depth);
+        }
+    }
+
+}
